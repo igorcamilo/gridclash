19c37f24b8359a81feb58a87b4c527d5a5186db6
Igor Camilo <11697241+igorcamilo@users.noreply.github.com>
2025-06-15 13:26:30 +0200

    Initialize project structure and add essential files for GridClash app

---
+++ GridClash/GridClashApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridClashApp.swift
+//  GridClash
+//
+//  Created by Igor Camilo on 15.06.25.
+//
+
+import SwiftUI
+
+@main
+struct GridClashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
